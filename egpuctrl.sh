e40cef6a18ac4e950298e8c7a1804bed07b2b720
cs97 <chrswr97@gmail.com>
2023-05-20 18:32:29 +0200

    Update egpuctrl.sh

--- egpuctrl.sh
+++ egpuctrl.sh
@@ -5,24 +5,7 @@
 # GPU Path
 path="/sys/class/drm/card1/device"
 
-
 #	you have to change the variables if you have a different gpu
-
-
-# my rx 5700 xt VDDC_CURVE
-
-#		OD_SCLK:
-#		0: 800Mhz
-#		1: 1900Mhz
-
-#		OD_MCLK:
-#		1: 875MHz
-
-#		OD_VDDC_CURVE:
-#		0: 800MHz 710mV
-#		1: 1417MHz 806mV
-#		2: 1900MHz 1001mV
-
 
 # OD_VDDC_CURVE:
 vc_1="1 1417 806"
